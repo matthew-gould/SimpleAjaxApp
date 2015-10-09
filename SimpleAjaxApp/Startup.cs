e00eef0c0b2396144b1f52adaea1e772364dbd8e
Matthew Gould <gouldm44@gmail.com>
2015-10-09 10:28:01 -0400

    Initial Commit

---
+++ SimpleAjaxApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleAjaxApp.Startup))]
+namespace SimpleAjaxApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
